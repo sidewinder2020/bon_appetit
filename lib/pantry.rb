a3ec33c356567ff95965e847c6b301d6298c4d36
sidewinder2020 <sdwinder1@gmail.com>
2017-08-01 09:18:37 -0600

    iteration-1 almost complete, minus the last test

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,30 @@
 class Pantry
 
+  def initialize
+    @pantry_stock = {}
+  end
+
+  def stock_check(food_name)
+    if @pantry_stock[food_name] != nil
+      @pantry_stock[food_name]
+    else
+      return 0
+    end
+  end
+
+  def restock(food_name, amount)
+    @pantry_stock[food_name] = amount
+  end
+
 end
+
+
+
+# pantry.stock_check("Cheese")
+# # => 0
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,22 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_you_can_check_stock
+    assert_equal 0, @pantry.stock_check("Cheese")
+  end
+
+  def test_you_can_add_stock_then_return_stock_check
+    @pantry.restock("Cheese", 10)
+
+    assert_equal 10, @pantry.stock_check("Cheese")
+  end
+
 end
